0f0151b87a22d1012e2c4b8c49171499248cd4ce
William Theodorus <williamwijaya499@gmail.com>
2025-07-16 18:15:51 +0700

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.16%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // 1. create a hash map to keep track of the num [key -> value]
         unordered_map<int, int> hash_map;
+
         for(int i = 0; i < nums.size(); i++){
-            int key = target - nums[i];
-            if(hash_map.count(key)){
-                return {hash_map[key], i};
+            // 2. store the number partner
+            int partner = target - nums[i];
+
+            // 3. check if the partner exists in the hashmap
+            if(hash_map.count(partner)) {
+                return {hash_map[partner], i};
             } else {
+                // 4. if not, store the other partner as the key and index as the value
                 hash_map[nums[i]] = i;
             }
         }
+
+        // 5. return an empty array if there are no correlated partner
         return {};
     }
 };
