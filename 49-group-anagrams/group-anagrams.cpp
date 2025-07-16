e5413e3acd7cfc14504bcf67887f016e5f815cde
William Theodorus <williamwijaya499@gmail.com>
2025-07-16 19:01:43 +0700

    Time: 14 ms (76.82%) | Memory: 25 MB (66.11%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    // hash function to sort the string (ex. "tea" -> "aet")
     string hash(string s) {
         sort(s.begin(), s.end());
         return s; 
     }
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> hash_map;
+        // 1. create a hash table instead of hash map to store vectors of string
+        unordered_map<string, vector<string>> hash_table;
+
+        // 2. create a 2D vector to store the results
         vector<vector<string>> res;
 
+        // 3. insert the value into the hash table
         for(string str : strs) {
             string key = hash(str);
-            hash_map[key].push_back(str);
+            hash_table[key].push_back(str);
         }
 
-        for(auto& val : hash_map) {
+        // 4. take the 2D vector that were stored
+        for(auto& val : hash_table) {
             res.push_back(val.second);
         }
 
+        // 5. return the 2D vector
         return res;
     }
 };
