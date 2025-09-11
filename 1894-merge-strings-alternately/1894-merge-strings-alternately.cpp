57b74c09e70ed8c3535676f5884cf8c202377330
William Theodorus <williamwijaya499@gmail.com>
2025-09-11 11:33:27 +0700

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 0 ms (100%), Space: 8.4 MB (83.18%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int length = word1.size() <= word2.size() ? word1.size() : word2.size();
-
+        int length = max(word1.size(), word2.size());
+        
         string res = "";
-        int last = 0;
         for (int i = 0; i < length; i++) {
-            res += word1[i];
-            res += word2[i];
-            last = i;
-        }
-
-        if (word1.size() > word2.size()) {
-            for (int i = last + 1; i < word1.size(); i++) {
+            if (i < word1.size()) {
                 res += word1[i];
             }
-        } else {
-            for (int i = last + 1; i < word2.size(); i++) {
+            if (i < word2.size()) {
                 res += word2[i];
             }
         }
+
         return res;
     }
 };
